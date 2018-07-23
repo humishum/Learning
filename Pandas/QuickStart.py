9dc89b35bd3392c50db738dcf04d9db61f52bbae
Humdaan Mustafa <humishum@gmail.com>
2018-07-23 13:05:53 -0700

    work

--- Pandas/QuickStart.py
+++ Pandas/QuickStart.py
@@ -1,8 +1,23 @@
+#referenced from :https://pandas.pydata.org/pandas-docs/stable/10min.html#
 #======= PANDAS ======
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 #==== Object creation====
+#Series
 s = pd.Series([1,3,5,np.nan,6,8])
 s
+#DataFrame
+dates = pd.data_range('20130101',periods=6)
+
+df = pd.DataFrame(np.random.randn(6,4), index=dates,columns =list('ABCD'))
+df
+
+df2 = pd.DataFrame({ 'A' : 1.,
+                     'B' : pd.Timestamp('20130102'),
+                     'C' : pd.Series(1,index=list(range(4)),dtype='float32'),
+                     'D' : np.array([3] * 4,dtype='int32'),
+                     'E' : pd.Categorical(["test","train","test","train"]),
+                     'F' : 'foo' })
+#df2.dtypes returns the fact that every column is a different type
